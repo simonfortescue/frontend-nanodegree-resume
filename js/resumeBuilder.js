b2d5b535ebb4d713d66dd7725ed2559aa5ba04ac
Simon Fortescue <simonfortescue@gmail.com>
2015-01-08 17:11:05 +1300

    added string replace funthoughts lesson 1 datatypes part 3

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,16 @@
-$("#main").append(["Simon Fortescue"]);
+//$("#main").append("Simon Fortescue");
+
+//[string].replace([old],[new])
+
+//var email = "simonfortescue@udacity.com";
+
+//var newEmail = email.replace("udacity","gmail");
+
+//console.log(email);
+//console.log(newEmail);
+
+var awesomeThoughts = "My name is Simon and I am AWESOME!";
+
+var funThoughts = awesomeThoughts.replace("AWESOME!","FUN");
+
+$("#main").append(funThoughts);
