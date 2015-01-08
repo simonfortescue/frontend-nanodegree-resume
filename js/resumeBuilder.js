dceff63d5a41be5167aab764fd749afb430dd6a8
Simon Fortescue <simonfortescue@gmail.com>
2015-01-08 21:48:22 +1300

    added name and role to data in ResumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -9,8 +9,18 @@
 //console.log(email);
 //console.log(newEmail);
 
-var awesomeThoughts = "My name is Simon and I am AWESOME!";
+// var awesomeThoughts = "My name is Simon and I am AWESOME!";
 
-var funThoughts = awesomeThoughts.replace("AWESOME!","FUN");
+//var funThoughts = awesomeThoughts.replace("AWESOME!","FUN");
 
-$("#main").append(funThoughts);
+//$("#main").append(funThoughts);
+var name = "Simon Fortescue";
+var role = "Entrepreneur";
+
+var formattedName = HTMLheaderName.replace("%data%",name);
+var formattedRole = HTMLheaderRole.replace("%data%",role);
+
+//below is important to prepend the first element in the line, second :)
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
