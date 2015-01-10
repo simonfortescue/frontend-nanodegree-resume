15891e3dc19b3b93936c61d8238975144502aaaf
Simon Fortescue <simonfortescue@gmail.com>
2015-01-11 11:36:27 +1300

    added object called Bio with all my details and then added all the details to the end of '#main' object

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -9,18 +9,45 @@
 //console.log(email);
 //console.log(newEmail);
 
+
+
+
 // var awesomeThoughts = "My name is Simon and I am AWESOME!";
 
 //var funThoughts = awesomeThoughts.replace("AWESOME!","FUN");
 
 //$("#main").append(funThoughts);
-var name = "Simon Fortescue";
-var role = "Entrepreneur";
 
-var formattedName = HTMLheaderName.replace("%data%",name);
-var formattedRole = HTMLheaderRole.replace("%data%",role);
+
+
+
+
+//adding my name:
+//var name = "Simon Fortescue";
+//var role = "Entrepreneur";
+
+//var formattedName = HTMLheaderName.replace("%data%",name);
+//var formattedRole = HTMLheaderRole.replace("%data%",role);
 
 //below is important to prepend the first element in the line, second :)
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
+//$("#header").prepend(formattedRole);
+//$("#header").prepend(formattedName);
 
+
+
+
+
+var skills = 
+["running", "jumping", "baa ing", "JS"];
+
+var bio = {
+	"name" : "Simon",
+	"role" : "Entrepreneur",
+	"contact" : "simonfortescue@gmail.com",
+	"picture_URL" : "http://static.guim.co.uk/sys-images/Guardian/Pix/pictures/2014/4/11/1397210130748/Spring-Lamb.-Image-shot-2-011.jpg",
+	"welcome_message" : "G'day - baa baa. And hello from me.",
+	"skills" : skills
+};
+
+$("#main").append(bio.name,"<br>",bio.role,"<br>",bio.contact,"<br>",bio.picture_URL,"<br>",bio.welcome_message,"<br>",bio.
+skills);
