0c468fbdf630a716045395186a75bc3d77803a6b
Simon <simonfortescue@gmail.com>
2015-01-12 13:24:12 +1300

    added code from end JS lesson 1 data types

---
+++ js/resumeBuilder.js
@@ -0,0 +1,66 @@
+var bio  = {
+	"name": "Simon  Fortescue",
+	"role": "entrepeneur",
+	"contacts": {
+		"mobile": "021 0000 000",
+		"email": "simonfortescue@gmail.com",
+		"github": "simon fortescue",
+		"location": "Auckland"
+	}
+	"welcome message": "efkn jrhbfwherbf whebfh",
+	"skills" : ["fun", "superhuman powers", "web design"]
+}
+
+var education = {
+  "schools" : [
+	{
+		"name": "Broadwater",
+		"cities": "Godalming",
+		"Majors": ["english", "Maths", "Physics"],
+		"Minors": ["Art", "Music"],
+		"graduation years": 1993,
+		"online course information": "none"
+	},
+	{
+		"name": "Godalming College",
+		"cities": "Godalming",
+		"Majors": ["english", "Maths", "Physics"],
+		"Minors": ["Art", "Music"],
+		"graduation years": 1990,
+		"online course information": "none"	
+	}
+  ]
+}
+
+var work = {
+	"jobs": [
+	{
+		"employer": "Mr Delivery",
+		"title": "Entrepeneur",
+		"dates": "forever",
+		"description": "efffffffffffffffffffffffrfger"
+	},
+	{	
+		"employer": "Mr Delivery 2",
+		"title": "Entrepeneur 2",
+		"dates": "forever 2",
+		"description": "2 efffffffffffffffffffffffrfger 2"
+
+	}
+	]
+}
+
+var projects = {
+	"projects": [
+		{
+		 "title": "sample project 1",
+		 "date": "2014",
+		 "description": "ewwwwwwwwwwwwwwwwwwwwwwwwwwwwwwf"
+		}
+	]
+
+}
+
+$("#main").append(work["position"]);
+$("#main").append(education.name);
+
